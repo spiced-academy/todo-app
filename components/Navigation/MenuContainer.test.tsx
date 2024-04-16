b6a5b013eb534f4daed6877d815382f01a6a4189
J-1000 <janredmann1@gmail.com>
2024-04-16 17:09:25 +0200

    add types for MenuContainer.test.ts

--- components/Navigation/MenuContainer.test.tsx
+++ components/Navigation/MenuContainer.test.tsx
@@ -4,23 +4,23 @@ import MenuContainer from "./MenuContainer";
 
 test("renders a heading", () => {
   render(<MenuContainer />);
-  const heading = screen.getByText(/menu/i);
+  const heading: HTMLElement = screen.getByText(/menu/i);
   expect(heading).toBeInTheDocument();
 });
 
 test("renders a search bar", () => {
   render(<MenuContainer />);
-  const searchbar = screen.getByRole("textbox", { name: /search/i });
+  const searchbar: HTMLElement = screen.getByRole("textbox", { name: /search/i });
   expect(searchbar).toBeInTheDocument();
 });
 
 test("renders a list called 'Lists' with 4 ListItems ", () => {
   render(<MenuContainer />);
-  const listHeading = screen.getByRole("heading", { name: /lists/i });
+  const listHeading: HTMLElement = screen.getByRole("heading", { name: /lists/i });
 
-  const list = screen.getByRole("list");
+  const list: HTMLElement = screen.getByRole("list");
 
-  const listItems = screen.getAllByRole("listitem");
+  const listItems: HTMLElement[] = screen.getAllByRole("listitem");
 
   expect(listHeading).toBeInTheDocument();
   expect(list).toBeInTheDocument();
@@ -29,12 +29,12 @@ test("renders a list called 'Lists' with 4 ListItems ", () => {
 
 test("renders a dark mode toggle switch ", () => {
   render(<MenuContainer />);
-  const darkToggle = screen.getByRole("checkbox", { name: /Dark Mode/i });
+  const darkToggle: HTMLElement = screen.getByRole("checkbox", { name: /Dark Mode/i });
   expect(darkToggle).toBeInTheDocument();
 });
 
 test.skip("renders a Fun mode toggle switch ", () => {
   render(<MenuContainer />);
-  const darkToggle = screen.getByRole("checkbox", { name: /Fun Mode/i });
-  expect(darkToggle).toBeInTheDocument();
+  const funToggle: HTMLElement = screen.getByRole("checkbox", { name: /Fun Mode/i });
+  expect(funToggle).toBeInTheDocument();
 });
