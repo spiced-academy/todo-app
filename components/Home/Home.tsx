175dd7755c51121d296a277a654286ce4c2b986a
Sven Hermann <sven@spiced-academy.com>
2024-04-17 18:38:48 +0200

    Create client component for HomePage

---
+++ components/Home/Home.tsx
@@ -0,0 +1,62 @@
+"use client";
+import MainContainer from "@/components/Navigation/MainContainer";
+import { Spinner, Box, Heading } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import Layout from "@/components/Layout/Layout";
+import TaskList from "@/components/TaskList/TaskList";
+import useSWR from "swr";
+import { useTaskStore } from "@/store";
+import AddTaskInput from "@/components/Task/AddTaskInput";
+import SetupModal from "@/components/Modal/Modal";
+import "./Home.module.css";
+
+const Home = () => {
+  const { data: tasks, isLoading, error } = useSWR("/api/tasks");
+
+  const setActiveList = useTaskStore((state) => state.setActiveList);
+  const setCountingTasks = useTaskStore((state) => state.setCountingTasks);
+  useEffect(() => {
+    setActiveList("TaskTango - Home Page");
+    tasks && setCountingTasks(tasks);
+  });
+
+  if (!tasks) {
+    return;
+  }
+
+  if (isLoading) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        position="absolute"
+      >
+        <Heading size="xl">Loading...</Heading>
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="teal.400"
+          size="xl"
+        />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return <div>failed to load</div>;
+  }
+
+  return (
+    <Layout title="TaskTango - Home Page">
+      <MainContainer mainTitle="All Tasks">
+        <SetupModal />
+        <AddTaskInput />
+        <TaskList tasks={tasks} />
+      </MainContainer>
+    </Layout>
+  );
+};
+
+export default Home;
