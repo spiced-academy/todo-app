d4383d0971e45d05f06a49e08115b5988fc364ea
Sven Hermann <sven@spiced-academy.com>
2024-04-17 15:56:40 +0200

    Add type parameters for sql related method calls

--- pages/api/tasks/[id].tsx
+++ pages/api/tasks/[id].tsx
@@ -1,25 +1,22 @@
 import { pool } from "@/db/pg_pool";
 import { NextApiRequest, NextApiResponse } from 'next';
 
-
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
   const { id } = request.query;
 
   if (!id) {
     return;
   }
-  // await dbConnect();
 
   if (request.method === "DELETE") {
-    await pool.query("DELETE FROM \"Tasks\" WHERE id = $1", [id])
-    // await Task.findByIdAndDelete(id);
+    await pool.query<Task>("DELETE FROM \"Tasks\" WHERE id = $1", [id])
 
     response.status(200).json({ message: "Success!" });
   }
 
   if (request.method === "PUT") {
     // const task = await Task.findById(id);
-    const task = (await pool.query("SELECT * FROM \"Tasks\" WHERE id = $1", [id])).rows[0]
+    const task = (await pool.query<Task>("SELECT * FROM \"Tasks\" WHERE id = $1", [id])).rows[0]
 
     if (!task) {
       response.status(404).json({ status: "Task not found" });
@@ -29,7 +26,7 @@ export default async function handler(request: NextApiRequest, response: NextApi
     // await Task.findByIdAndUpdate(id, {
     //   $set: { title: request.body },
     // });
-    await pool.query("UPDATE \"Tasks\" SET title = $1 WHERE id = $2", [request.body.title, id] )
+    await pool.query<Task>("UPDATE \"Tasks\" SET title = $1 WHERE id = $2", [request.body.title, id])
 
     response.status(200).json({
       status: `Task ${id} was successfully edited!`,
@@ -37,22 +34,14 @@ export default async function handler(request: NextApiRequest, response: NextApi
   }
 
   if (request.method === "PATCH") {
-    // let task = await Task.findById(id);
-    const task = (await pool.query("SELECT * FROM \"Tasks\" WHERE id = $1", [id])).rows[0]
+    const task = (await pool.query<Task>("SELECT * FROM \"Tasks\" WHERE id = $1", [id])).rows[0]
 
     if (!task) {
       response.status(404).json({ status: "Task not found" });
       return;
     }
 
-    // task = await Task.findByIdAndUpdate(
-    //   id,
-    //   {
-    //     $set: { completed: !task.completed },
-    //   },
-    //   { new: true }
-    // );
-    await pool.query("UPDATE \"Tasks\" SET completed = $1 WHERE id = $2", [!task.completed, id] )
+    await pool.query<Task>("UPDATE \"Tasks\" SET completed = $1 WHERE id = $2", [!task.completed, id])
 
     response.status(200).json(task);
   }
--- pages/api/tasks/index.tsx
+++ pages/api/tasks/index.tsx
@@ -1,30 +1,24 @@
 import { pool } from "@/db/pg_pool";
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import dbConnect from '@/db/connect';
-
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
-  await dbConnect();
-
-  // await client.connect()
   if (request.method === "GET") {
-    // const tasks = await Task.find().sort("-created_at");
-    const result = await pool.query("SELECT * FROM \"Tasks\" ORDER BY created_at DESC")
+    const result = await pool.query<Task>("SELECT * FROM \"Tasks\" ORDER BY created_at DESC")
     const tasks = result.rows
-    console.log(result.rows);
+
     return response.status(200).json(tasks);
   }
 
   if (request.method === "POST") {
     try {
-      const {title: taskTitle} = request.body;
-      const result = await pool.query("INSERT INTO \"Tasks\" (title) VALUES ($1)", [taskTitle])
+      const { title: taskTitle } = request.body;
+      const result = await pool.query<Task>("INSERT INTO \"Tasks\" (title) VALUES ($1)", [taskTitle])
       const record = result.rows[0]
-      // const task = new Task(taskTitle);
-      // const record = await task.save();
+
       return response.status(201).json(record);
     } catch (error) {
       console.error(error);
+
       return response.status(400).json({ error: error instanceof Error ? error.message : "An unknown error occurred" });
     }
   }
