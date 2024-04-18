ba6996c799e7cb12b0c92b76dd32a709ba465006
Sven Hermann <sven@spiced-academy.com>
2024-04-19 00:31:26 +0200

    Use custom theme for chacra ui

--- providers/chacra-provider.tsx
+++ providers/chacra-provider.tsx
@@ -1,6 +1,7 @@
 'use client'
-import { ChakraProvider as CP, theme } from "@chakra-ui/react";
+import { ChakraProvider as CP } from "@chakra-ui/react";
 import React, { ReactNode } from 'react';
+import { theme } from "@/theme";
 
 export const ChakraProvider = ({ children }: { children: ReactNode }) => {
     return <CP theme={theme}>
