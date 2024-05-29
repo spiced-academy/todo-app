958ea023d63eed4a29bb240fdccb849a0811286d
Sven Hermann <sven@spiced-academy.com>
2024-05-29 09:57:05 +0200

    Show number of unread messages as badge

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,6 @@
 "use client"
-import React from 'react';
-import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { Avatar, Badge, Box, Button, Flex, Text } from '@chakra-ui/react';
 import { signIn, signOut } from 'next-auth/react';
 import type { Session } from 'next-auth';
 
@@ -9,6 +9,7 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ session }) => {
+  const [unreadMessages, setUnreadMessages] = useState(0)
 
   function onLogin() {
     signIn()
@@ -19,6 +20,22 @@ const Header: React.FC<HeaderProps> = ({ session }) => {
   }
 
   const isLoggedIn = session?.user
+
+  useEffect(() => {
+    const eventSource = new EventSource('/api/messages');
+    function sendEvent(event: MessageEvent) {
+      const unreadMessages = JSON.parse(event.data).unreadMessages
+      console.log(unreadMessages)
+      setUnreadMessages(unreadMessages)
+    }
+
+    eventSource.addEventListener('message', sendEvent);
+
+    return () => {
+      eventSource.removeEventListener('message', sendEvent);
+    };
+
+  }, [])
 
   return (
     <Flex as="nav" align="center" justify="space-between" wrap="wrap" padding="1.5rem" bg="teal.500" color="white">
@@ -32,6 +49,14 @@ const Header: React.FC<HeaderProps> = ({ session }) => {
         {isLoggedIn ? (
           <>
             <Text mr={4}>Welcome, {session?.user?.name}</Text>
+            <Box as="a" href="/profile" mr={4}>
+              <Box position="relative">
+                <Avatar name={session?.user?.name || ""} src={session?.user?.image || ""} />
+                <Badge colorScheme="red" position="absolute" top="0" right="0" variant="solid" borderRadius="full">
+                  {unreadMessages > 0 ? unreadMessages : ''}
+                </Badge>
+              </Box>
+            </Box>
             <Button onClick={onLogout} bg="transparent" border="1px">
               Logout
             </Button>
