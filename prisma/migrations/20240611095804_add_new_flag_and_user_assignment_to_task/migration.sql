ad463ac8406734039909f1d4a8b916d18c963e08
Sven Hermann <sven@spiced-academy.com>
2024-06-11 11:59:04 +0200

    Add "new" flag and user assignment to Task

---
+++ prisma/migrations/20240611095804_add_new_flag_and_user_assignment_to_task/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Added the required column `user_id` to the `Task` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Task" ADD COLUMN     "new" BOOLEAN DEFAULT true,
+ADD COLUMN     "user_id" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
