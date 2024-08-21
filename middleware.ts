b3917a058293f47cd15ebe15a79f02a28b699090
Sven Hermann <sven@spiced-academy.com>
2024-08-21 12:47:08 +0000

    Add BearerAuth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,15 @@
 export { default } from "next-auth/middleware"
+import BearerAuthMiddleware from "./middlewares/BearerAuthMiddleware"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function middleware(req: NextRequest, res: NextResponse) {
+    return BearerAuthMiddleware(req, res)
+}
 
 export const config = { 
     matcher: [
         "/tasks/:path*",
+        "/api/:path*"
     ] 
 }
 
