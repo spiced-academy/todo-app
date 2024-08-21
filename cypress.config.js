f4763dc2692ed70a734f4e5505457f055956d146
Sven Hermann <sven@spiced-academy.com>
2024-08-21 14:07:55 +0000

    Disable cypress video creation

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   defaultCommandTimeout: 20000,
+  video: false,
   e2e: {
     baseUrl: 'http://nextjs:3000',
     supportFile: false,
