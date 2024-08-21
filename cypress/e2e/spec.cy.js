1072d5edc578ef5d2a9e9b5b8e0770d75f880ddc
Sven Hermann <sven@spiced-academy.com>
2024-08-21 11:27:10 +0200

    Add tests for login page

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,22 @@
+describe('Login Page', () => {
+    it('should display the login page', () => {
+      cy.visit('/login')
+      cy.contains('Login')
+    })
+
+    it('should log me in, when I enter the correct credentials', () => {
+        cy.visit('/login')
+        cy.get('input[type="email"]').type('sven@svenhermann.it')
+        cy.get('input[type="password"]').type('sven')
+        cy.get('button[type="submit"]').click()
+        cy.contains('Welcome, Sven Hermann')
+    })
+
+    it('should not log me in, when I enter the wrong credentials', () => {
+        cy.visit('/login')
+        cy.get('input[type="email"]').type('sven@svenhermann.it')
+        cy.get('input[type="password"]').type('wrong')
+        cy.get('button[type="submit"]').click()
+        cy.contains('Invalid password')
+    })
+  })
